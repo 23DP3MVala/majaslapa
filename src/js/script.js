6352e032c5258cd97c30384c00356fd452e0d772
Marta Aleksa Valaine <a230440mv@rvt.lv>
2025-02-23 21:33:36 +0200

    Pabeigts projekts

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,17 @@
 document.getElementById("poga_par_skyome").addEventListener("click", function () {
-    window.location.href="#par_skyome"
-})
+    window.location.href="#par_skyome";
+});
+
+async function fact() {
+    const response = await fetch("https://catfact.ninja/fact");
+    const data = await response.json();
+    document.getElementById("izvade").textContent = data.fact;
+};
 
 document.getElementById("cat_fact").addEventListener("click", function () {
-    document.getElementById("izvade").innerHTML = "aa";
-})
+    fact();
+});
+
+document.getElementById("menesis").addEventListener("change", function() {
+    document.getElementById("ikona").src = this.value;
+});
